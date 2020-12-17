8103efcd31d3ece686df47c1da42aa30525a5bbd
Himanshu Verkiya <hiverkiya@gmail.com>
2020-12-17 19:18:04 +0530

    Adding animation code

--- main.py
+++ main.py
@@ -34,7 +34,39 @@ class Bird:
         self.height=self.y #where bird jumped from 
 
     def move(self):
+        self.tick_count+=1
 
+        d = self.vel*self.tick_count+1.5*self.tick_count**2 #Displacement up or down for our bird
+
+        if d>=16: #Our terminal velocity
+            d = 16
+        if d<0: #Jump height
+            d-=2    
+        self.y=self.y + d
+
+        if d<0 or self.y<self.height+50:
+            if self.tilt<self.MAX_ROTATION:
+                self.tilt=self.MAX_ROTATION
+        else:
+            if self.tilt>-90:
+                self.tilt -=self.ROT_VEL
+    def draw(self,win): #Showing Flappy Bird images based on condition and animating them
+        self.img_count+=1
+
+        if self.img_count<self.ANIMATION_TIME:
+            self.img=self.IMGS[0]
+        elif self.img_count<self.ANIMATION_TIME*2:
+            self.img=self.IMGS[1]
+        elif self.img_count<self.ANIMATION_TIME*3:
+            self.img=self.IMGS[2]    
+        elif self.img_count<self.ANIMATION_TIME*4:
+            self.img=self.IMGS[1]
+        elif self.img_count==self.ANIMATION_TIME*4+1:
+            self.img=self.IMGS[0]
+            self.img_count=0
+
+        if self.tilt<=-180: #Not flapping when going down
+            self.img=self.IMGS[1]
+            self.img_count=self.ANIMATION_TIME*2        
 while True:#we kinda specifying fps here ,well its also kind of gameloop
     bird.move()
-    
